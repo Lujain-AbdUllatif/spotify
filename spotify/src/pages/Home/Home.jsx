0cb78cde52ec7347d76fa3b033a2d1679f931f80
Lujain <lujain.abdallatif@mail.huji.ac.il>
2021-04-13 16:14:08 +0300

    home page almost done

--- spotify/src/pages/Home/Home.jsx
+++ spotify/src/pages/Home/Home.jsx
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { Component, useEffect } from "react";
 
+// API
+import { recentlyPlayed, featuredPlaylists, mood } from "../../API/homeAPI";
+
+// Components
 import NavBar from "../../components/NavBar/NavBar";
+import PlayList from "../../components/PlayList/PlayList";
+import Carousel from "../../components/Carousel/Carousel";
 
 import "./home.css";
 
 export default function Home() {
+  let [recentlyPlayedData, setRecentlyPlayedData] = React.useState();
+  let [featuredPlaylistsData, setFeaturedPlaylistsData] = React.useState();
+  let [moodData, setMoodData] = React.useState();
+
+  useEffect(() => {
+    recentlyPlayed().then((data) => {
+      setRecentlyPlayedData(data);
+      console.log(featuredPlaylistsData);
+    });
+    featuredPlaylists().then((data) => setFeaturedPlaylistsData(data));
+    mood().then((data) => setMoodData(data));
+  }, []);
+
   return (
     <div className="home-container">
-      <NavBar homeState={true}></NavBar>
-      <div className="home-div">Home Page</div>
+      <NavBar active="home"></NavBar>
+      <div className="home-div">
+        {!recentlyPlayedData ? (
+          <Carousel
+            title="Recently Played"
+            data={recentlyPlayedData}
+          ></Carousel>
+        ) : (
+          ""
+        )}
+        <Carousel
+          title="Featured playlists"
+          data={featuredPlaylistsData}
+        ></Carousel>
+        <Carousel title="Mood" data={moodData}></Carousel>
+      </div>
     </div>
   );
 }
